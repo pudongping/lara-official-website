e2440340e58f655d10a89bd727d4cb91a09eb359
Alex <1414818093@qq.com>
2020-06-17 15:53:42 +0800

    处理 video 标签问题

--- app/Observers/Product/ProductSpuObserver.php
+++ app/Observers/Product/ProductSpuObserver.php
@@ -15,8 +15,8 @@ class ProductSpuObserver
 
     public function saving(ProductSpu $productSpu)
     {
-        // 修复 XSS 注入漏洞
-        $productSpu->description = clean($productSpu->description, 'spu_description');
+        // 修复 XSS 注入漏洞 （因为目前插件不支持过滤 html5 指定标签，且此处只会用到后台管理，风险不大，故注释掉以下代码）
+//        $productSpu->description = clean($productSpu->description, 'spu_description');
     }
 
 }
